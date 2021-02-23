de166736494e4e3281743ae1f6fe156201e3bbb6
maryd-k <maryia.dyliuk@gmail.com>
2021-02-23 16:52:52 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
+  let newN;
+    if (n<0)
+  {
+      newN=Math.abs(n);
+      n=newN;
+  }
+  let mas=n.toString().split("");
+  let num;
+  for (let i=0, j=mas.length - 1; i<Math.trunc(mas.length/2) && j>=Math.ceil(mas.length/2); i++, j--)
+  {
+      num=mas[i];
+      mas[i]=mas[j];
+      mas[j]=num;
+  }
+  num = Number(mas.join("")); 
+  return num;
 }
